b3e2a44add60de10679e8c1f330fd18b6bc10f2f
Hiep Le <>
2023-10-20 00:37:03 +0700

    feat: [contract] simple number counter

--- contract/src/contract.ts
+++ contract/src/contract.ts
@@ -1,6 +1,40 @@
+import { CallTracker } from 'assert';
 import { NearBindgen, near, call, view } from 'near-sdk-js';
 
 @NearBindgen({})
 class Contract {
-  
+  num: number = 0
+
+  @view({})
+  get_number(): number {
+    return this.num;
+  }
+
+  @call({})
+  increase_number(): void {
+    near.log("Increasing number by " + near.predecessorAccountId());
+    this.num++;
+  }
+
+  @call({})
+  decrease_number(): void {
+    if (this.num == 0) {
+      throw new Error("Can't decrease number")
+    }
+
+    // decrease number and log account call
+    near.log("Decrease number by " + near.predecessorAccountId());
+    this.num--;
+  }
+
+  @call({ privateFunction: true })
+  reset_number(): void {
+    this.num = 0;
+  }
+
+  @call({ payableFunction: true })
+  payable_function(): void {
+    near.log("User deposit amount: " + near.attachedDeposit());
+    this.num += 10;
+  }
 }
